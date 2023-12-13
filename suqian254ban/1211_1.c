ed54432f7467ed1a3f2a7dd6e302c9a3d7148534
挡泥板 <1573465644@qq.com>
2023-12-13 09:28:55 +0800

    [新增]作业

--- suqian254ban/1211_1.c
+++ suqian254ban/1211_1.c
@@ -3,15 +3,15 @@
 #include <time.h>
 
 #define ARRAY_SiZE 10
-// 数组快排
+#if 0 // 作业1， 数组快排
 void fastSost(int *date, int start, int len)
 {
-    if (start >= len)
+    if (start >= len-1)
     {
         return;
     }
     int left = start;
-    int right = len;
+    int right = len-1;
     int temp = date[left];
     while (left < right)
     {
@@ -36,10 +36,11 @@ void fastSost(int *date, int start, int len)
     }
     date[left] = temp;
     fastSost(date, 0, left - 1);
-    fastSost(date, right + 1, len);
+    fastSost(date, right + 1, len-1);
 }
+#endif
 
-// 数组去重
+#if 0 // 作业2，有序数组去重
 void Deduplication(int *Random, int end)
 {
     int count=0;
@@ -50,21 +51,43 @@ void Deduplication(int *Random, int end)
         Random[indx+1-count]=Random[indx+1];
     }
     end -=count;
-    for (int idx = 0; idx < end+1; idx++)
+    for (int idx = 0; idx < end; idx++)
     {
         printf("%d ", Random[idx]);
     }
     printf("\n");
     printf("len:%d\n", end+1);
 }
+#endif
 
+#if 1 // 作业3，删除数组指定元素
+void DeleteElement(int *ptr, int end, int element)
+{
+    int count = 0;
+    for (int idx = 0; idx < end; idx++)
+    {
+        if (ptr[idx] == element)
+            count += 1;
+        ptr[idx + 1 - count] = ptr[idx + 1];
+    }
+    end -= count;
+
+    for (int idx = 0; idx < end; idx++)
+    {
+        printf("%d ", ptr[idx]);
+    }
+    printf("\n");
+    printf("len:%d\n", end);
+}
+#endif
 
 int main()
 {
     srand(time(NULL));
     int Random[ARRAY_SiZE] = {0};
+    int target = 0;
     // memset(Random, 0, sizeof(int) * ARRAY_SiZE);
-
+    // 生成随机数组并输出
     for (int idx = 0; idx < ARRAY_SiZE; idx++)
     {
         Random[idx] = rand() % ARRAY_SiZE;
@@ -72,17 +95,19 @@ int main()
     }
     printf("\n");
 
-    int len = sizeof(Random) / 4 - 1;
+    int len = sizeof(Random) / 4;
     printf("len:%d\n", len);
-    fastSost(Random, 0, len);
-    for (int idx = 0; idx < ARRAY_SiZE; idx++)
-    {
-        printf("%d ", Random[idx]);
-    }
-    printf("\n");
 
-    Deduplication(Random, len);
-    
+    // fastSost(Random, 0, len);
+    // for (int idx = 0; idx < ARRAY_SiZE; idx++)
+    // {
+    //     printf("%d ", Random[idx]);
+    // }
+    // printf("\n");
 
+    // Deduplication(Random, len);
+    printf("\n请输入需要删除的数!\n");
+    scanf("%d", &target);
+    DeleteElement(Random, len, target);
     return 0;
 }
--- suqian254ban/1211_2.c
+++ suqian254ban/1211_2.c
@@ -4,4 +4,14 @@
 int main()
 {
 
+    char*s[10];
+    s[0]="suqian";
+    s[1]="jiaoyu";
+    printf("%c\n",*s[0]);
+    printf("%c\n",*(s[0]+1));
+    printf("%c\n",*s[0]+1);
+    printf("%c\n",*(s[0]+6));
+    printf("%c\n",*(s[0]+7));
+    printf("%c\n",**(&s[0]+1));
+
 }
